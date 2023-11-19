6bc1de73614aefe5bd98cddb39fe3a81ef69b52e
vspoke91 <victorvjro91@gmail.com>
2023-11-19 13:06:14 -0500

    ADD: header z-index and update home section layout

--- src/Home.jsx
+++ src/Home.jsx
@@ -24,7 +24,7 @@ import spaceman from "./assets/spaceman.svg";
 function Home() {
   return (
     <>
-      <header className="bg-[rgb(58,58,58)] flex h-[60px] fixed inset-x-0 top-0">
+      <header className="bg-[rgb(58,58,58)] flex h-[60px] fixed inset-x-0 top-0 z-50">
         <h1 className="font-Agbalumo text-3xl ml-[2vw] cursor-default my-auto">
           Victor R.O
         </h1>
@@ -57,11 +57,68 @@ function Home() {
         </button>
       </header>
       <main className="mt-[60px]">
-        <div id="home" className="flex justify-center min-h-screen">
-          <div className="flex-1">
-            <h2>Victor Romero</h2>
-            <p>dawdaw daw daw faw f aw fawf aw fawf aw faw f aw fa wf aw </p>
+        {/* [Home] */}
+        <div
+          id="home"
+          className="flex justify-center min-h-[calc(100vh-60px)] items-center"
+        >
+          {/* intro */}
+          <div className="flex-1 ml-[5vh] mr-[1vh]">
+            <h2>
+              <span className="block text-3xl font-libre font-light">
+                Hello, My name is
+              </span>
+              <strong className="text-4xl font-libre">Victor Romero</strong>
+            </h2>
+            <p className="mt-3 w-[90%]">
+              I&apos;m a software developer, I love coding and I&apos;m always
+              looking forward to learn new things.
+            </p>
+            {/* -media- */}
+            <ul className="flex gap-4 w-full mt-3">
+              <li>
+                <a href="">
+                  <FontAwesomeIcon
+                    icon={faLinkedin}
+                    className="h-[35px] w-auto"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <FontAwesomeIcon
+                    icon={faGithub}
+                    className="h-[35px] w-auto"
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <FontAwesomeIcon
+                    icon={faEnvelope}
+                    className="h-[35px] w-auto"
+                  />
+                </a>
+              </li>
+            </ul>
+            {/* -achievements- */}
+            <h3 className="text-xl font-mono font-black mx-auto w-fit mt-8">
+              Achievements
+            </h3>
+            <ul className="flex justify-center gap-4 mt-3">
+              <li>
+                <a href="">
+                  <FontAwesomeIcon icon={faWindows} className="h-[35px]" />
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <FontAwesomeIcon icon={faFreeCodeCamp} className="h-[35px]" />
+                </a>
+              </li>
+            </ul>
           </div>
+          {/* SVG */}
           <div className="flex-1">
             <img
               className="h-[auto] w-[30vw]"
@@ -69,20 +126,48 @@ function Home() {
               alt="svg of spaceman"
             />
           </div>
-          <div className="flex-1">
-            <h2>Knowledge Hub</h2>
-            <ul>
-              <li>
-                <i></i>
-                <span>HTML, JavaScript, CSS</span>
+          {/* Knowledge */}
+          <div className="flex-1 relative z-0">
+            <h2 className="text-2xl absolute -top-[10vh]">
+              <span className="font-bold font-libre">Knowledge Hub</span>
+              <FontAwesomeIcon icon={faGraduationCap} className="ml-1" />
+            </h2>
+            <ul className="flex flex-col gap-[3vw] w-fit">
+              <li className="flex text-5xl items-center -translate-x-6">
+                <button>
+                  <FontAwesomeIcon icon={faGitAlt} />
+                  <span className="text-xl ml-1 hidden">Git</span>
+                </button>
               </li>
-              <li>
-                <i></i>
-                <span>Python</span>
+              <li className="flex text-3xl items-center">
+                <button>
+                  <div className="flex gap-1">
+                    <FontAwesomeIcon icon={faHtml5} />
+                    <FontAwesomeIcon icon={faSquareJs} />
+                    <FontAwesomeIcon icon={faCss3Alt} />
+                  </div>
+                </button>
+                <span className="text-xl ml-1 hidden">
+                  HTML / JavaScript / CSS
+                </span>
               </li>
-              <li>
-                <i></i>
-                <span>C#</span>
+              <li className="flex text-5xl items-center translate-x-6">
+                <button>
+                  <FontAwesomeIcon icon={faPython} />
+                  <span className="text-xl ml-1 hidden">Python</span>
+                </button>
+              </li>
+              <li className="flex text-5xl items-center">
+                <button>
+                  <FontAwesomeIcon icon={faJava} />
+                  <span className="text-xl ml-1 hidden">Java</span>
+                </button>
+              </li>
+              <li className="flex items-center -translate-x-6">
+                <button>
+                  <img className="h-[45px]" src={csharp} alt="csharp logo" />
+                  <span className="text-xl ml-1 hidden">C-Sharp</span>
+                </button>
               </li>
             </ul>
           </div>
