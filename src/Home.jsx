afad0accaf2d305075f865da875171de3c780c94
vspoke91 <victorvjro91@gmail.com>
2023-11-18 19:39:13 -0500

    CHNG: header and navigation styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,28 +5,39 @@ import spaceman from "./assets/spaceman.svg";
 function Home() {
   return (
     <>
-      <header>
-        <h1 className="font-Agbalumo">VJRO</h1>
-        <nav>
-          <ul>
-            <li>
-              <a href="#home">
-                <FontAwesomeIcon icon={faHouse} />
-                <span>Home</span>
+      <header className="bg-[rgb(58,58,58)] flex h-[60px] fixed inset-x-0 top-0">
+        <h1 className="font-Agbalumo text-3xl ml-[2vw] cursor-default my-auto">
+          Victor R.O
+        </h1>
+        <div className="border-[2px] rounded-lg h-[75%] mx-4 my-auto" />
+        <nav className="flex gap-4 h-full item-center">
+          <ul className="contents">
+            <li className="contents">
+              <a href="#home" className="h-full flex items-center">
+                <FontAwesomeIcon icon={faHouse} className=" mr-1" />
+                <span className="font-bold">Home</span>
               </a>
-              <a href="#projects">
-                <FontAwesomeIcon icon={faCode} />
-                <span>Projects</span>
+            </li>
+            <li className="contents">
+              <a href="#projects" className="h-full flex items-center">
+                <FontAwesomeIcon icon={faCode} className=" mr-1" />
+                <span className="font-bold">Projects</span>
               </a>
-              <a href="#about">
-                <FontAwesomeIcon icon={faUser} />
-                <span>About</span>
+            </li>
+            <li className="contents">
+              <a href="#about" className="h-full flex items-center">
+                <FontAwesomeIcon icon={faUser} className=" mr-1" />
+                <span className="font-bold">About</span>
               </a>
             </li>
           </ul>
         </nav>
+        <div className="grow" />
+        <button className="bg-green-700 rounded-lg h-fit my-auto mr-[10vh] font-bold py-2 px-3 text-lg font-mono">
+          Contact Me
+        </button>
       </header>
-      <main>
+      <main className="mt-[60px]">
         <div id="home" className="flex justify-center min-h-screen">
           <div className="flex-1">
             <h2>Victor Romero</h2>
@@ -42,10 +53,18 @@ function Home() {
           <div className="flex-1">
             <h2>Knowledge Hub</h2>
             <ul>
-              <li>HTML, Javascript, CSS</li>
-              <li>Java</li>
-              <li>Python</li>
-              <li>C#</li>
+              <li>
+                <i></i>
+                <span>HTML, JavaScript, CSS</span>
+              </li>
+              <li>
+                <i></i>
+                <span>Python</span>
+              </li>
+              <li>
+                <i></i>
+                <span>C#</span>
+              </li>
             </ul>
           </div>
         </div>
