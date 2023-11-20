04be7cfa898b691b53b7ac2e52b3eec6d6408d7c
vspoke91 <victorvjro91@gmail.com>
2023-11-20 17:01:29 -0500

    ADD: KnowledgeDatabase.js with project information

---
+++ src/data/KnowledgeDatabase.js
@@ -0,0 +1,71 @@
+const projects = {
+  aiworld: {
+    name: "AI World",
+    image: "https://aiworld.veltaproject.com/img/logos/AI-World-Large.png",
+    link: "www.project1.com",
+    githubRepo: "github.com/username/project1",
+  },
+  aiworldx: {
+    name: "AI Worlddw",
+    image: "https://aiworld.veltaproject.com/img/logos/AI-World-Large.png",
+    link: "www.project1.com",
+    githubRepo: "github.com/username/project1",
+  },
+};
+
+export default {
+  git: {
+    name: "Git",
+    level: "Proficient",
+    usedTime: "1 years or Less",
+    acquared: "Self Taught",
+    showcase: [projects.aiworld, projects.aiworldx],
+  },
+  web: {
+    name: "HTML/CSS/JavaScript",
+    level: "Expert",
+    usedTime: "1 years or Less",
+    acquared: "Self Taught",
+    technologies: [],
+    showcase: [
+      {
+        name: "Project 1",
+        link: "www.project1.com",
+        githubRepo: "github.com/username/project1",
+      },
+    ],
+  },
+  python: {
+    name: "Python",
+    level: "Novice",
+    usedTime: "1 years or Less",
+    acquared: "Self Taught",
+    showcase: [
+      {
+        name: "Project 1",
+        link: "www.project1.com",
+        githubRepo: "github.com/username/project1",
+      },
+    ],
+  },
+  java: {
+    name: "Java",
+    level: "Expert",
+    usedTime: "2 years to 3 years",
+    acquared: "Schooled",
+    showcase: [projects.aiworld],
+  },
+  csharp: {
+    name: "C#",
+    level: "Competent",
+    usedTime: "1 years or Less",
+    acquared: "Self Taught",
+    showcase: [
+      {
+        name: "Project 1",
+        link: "www.project1.com",
+        githubRepo: "github.com/username/project1",
+      },
+    ],
+  },
+};
