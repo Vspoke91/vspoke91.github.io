1571236fbd8b45087688bf5347a071e445cae59c
vspoke91 <victorvjro91@gmail.com>
2023-11-28 13:31:39 -0500

    ADD: faEye icon to HorizontalScroll component

--- src/components/customs/HorizontalScroll.jsx
+++ src/components/customs/HorizontalScroll.jsx
@@ -1,6 +1,10 @@
 // Icons Imports
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretRight, faCaretLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCaretRight,
+  faCaretLeft,
+  faEye,
+} from "@fortawesome/free-solid-svg-icons";
 
 // React Imports
 import PropTypes from "prop-types";
@@ -11,40 +15,6 @@ export default function Default({ children, className, controlsClassName }) {
   const [target, setTarget] = useState(null);
   const [childrenRender, setChildrenRender] = useState(null);
   const holderRef = useRef(null);
-
-  // useEffect(() => {
-  //   // Event Handlers
-  //   function targetClickHandler(e) {
-  //     const targetChild = holderRef.current.children[target];
-  //     const newTargetIndex = parseInt(e.target.dataset.index);
-
-  //     if (targetChild && target !== newTargetIndex) {
-  //       targetChild.removeAttribute("style");
-  //     }
-  //     setTarget(newTargetIndex);
-  //   }
-
-  //   // Children Render
-  //   const childrenList = [];
-
-  //   if (children && children.length > 1) {
-  //     children.forEach((child, index) =>
-  //       childrenList.push(
-  //         <li
-  //           key={index}
-  //           data-index={index}
-  //           className="grw-0 shrink-0 scale-90"
-  //           onClick={targetClickHandler}
-  //         >
-  //           {child}
-  //         </li>
-  //       )
-  //     );
-  //   } else if (children) {
-  //     childrenList.push(<li>{children}</li>);
-  //   }
-  //   setChildren(childrenList);
-  // }, [children, target]);
 
   useEffect(() => {
     const targetChild = holderRef.current.children[target];
@@ -63,10 +33,11 @@ export default function Default({ children, className, controlsClassName }) {
             <li
               key={index}
               data-index={index}
-              className="grw-0 shrink-0 scale-90 transition-all"
+              className="grw-0 shrink-0 scale-90 transition-all cursor-pointe select-none"
               onClick={(e) => targetClickHandler(e)}
             >
               {element}
+              <FontAwesomeIcon icon={faEye} className="hidden" />
             </li>
           )
         );
@@ -92,8 +63,35 @@ export default function Default({ children, className, controlsClassName }) {
     }
   }, [target, children]);
 
+  //scroll functions
+
+  const [isDragging, setIsDragging] = useState(false);
+  const [prevPos, setPrevPos] = useState({ x: 0, y: 0 });
+
+  const dragMouseUpHandler = (e) => {
+    holderRef.current.classList.remove("cursor-grab");
+    setIsDragging(false);
+  };
+  const dragMouseMoveHandler = (e) => {
+    if (!isDragging) return;
+    holderRef.current.scrollLeft += prevPos.x - e.clientX;
+    holderRef.current.scrollTop += prevPos.y - e.clientY;
+    setPrevPos({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+  const dragMouseDownHandler = (e) => {
+    holderRef.current.classList.add("cursor-grab");
+    setIsDragging(true);
+    setPrevPos({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  };
+
   return (
-    <div className="relative ">
+    <div className="relative">
       <div className="absolute w-full flex justify-between px-2 h-full pointer-events-none">
         <button className="pointer-events-auto h-fit my-auto">
           <FontAwesomeIcon icon={faCaretLeft} className={controlsClassName} />
@@ -102,7 +100,13 @@ export default function Default({ children, className, controlsClassName }) {
           <FontAwesomeIcon icon={faCaretRight} className={controlsClassName} />
         </button>
       </div>
-      <ul className={className} ref={holderRef}>
+      <ul
+        className={className}
+        ref={holderRef}
+        onMouseDown={(e) => dragMouseDownHandler(e)}
+        onMouseMove={(e) => dragMouseMoveHandler(e)}
+        onMouseUp={(e) => dragMouseUpHandler(e)}
+      >
         {childrenRender}
       </ul>
     </div>
