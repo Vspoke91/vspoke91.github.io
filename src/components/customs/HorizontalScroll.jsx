5e39ee2dfd796b703ddb385f5b22a4d924180f77
vspoke91 <victorvjro91@gmail.com>
2023-12-08 16:34:04 -0500

    CHNG: Horizontal scroll functionality and style

--- src/components/customs/HorizontalScroll.jsx
+++ src/components/customs/HorizontalScroll.jsx
@@ -10,197 +10,121 @@ import {
 import PropTypes from "prop-types";
 import { useEffect, useState, useRef } from "react";
 
-export default function Default({
-  children,
-  className,
-  controlsClassName,
-  onClick,
-  reRenderOnChange,
-}) {
+export default function Default({ children }) {
   //React Hooks
   const [childrenRender, setChildrenRender] = useState(null);
+  const scrollHolderRef = useRef(null);
   const holderRef = useRef(null);
-  const controlsRef = useRef({ right: null, left: null });
+  const controlsRef = useRef({ left: null, right: null });
 
+  //create li elements from children
   useEffect(() => {
-    const childrenArray = [];
-
+    //create li elements
     if (children && children.length) {
-      if (children.length) {
-        children.map((element, index) =>
-          childrenArray.push(itemElement(index, element))
-        );
-      }
-      setChildrenRender(childrenArray);
+      const elementChildrenArray = [];
+
+      children.map((childElement, index) =>
+        elementChildrenArray.push(createLiElement(index, childElement)),
+      );
+      setChildrenRender(elementChildrenArray);
+    } else {
+      setChildrenRender(<></>);
     }
-
-    function itemElement(index, content) {
-      let isClicking = false;
-      let movedPos = 0;
-      let startPos = 0;
-
-      const clickingLeaveHandler = () => {
-        isClicking = false;
-      };
-      const clickingUpHandler = () => {
-        if (movedPos < 20) onClick();
-        isClicking = false;
-      };
-      const clickingMoveHandler = (e) => {
-        if (!isClicking) return;
-        movedPos += Math.abs(startPos - e.clientX);
-      };
-      const clickingDownHandler = (e) => {
-        isClicking = true;
-        movedPos = 0;
-        startPos = e.clientX;
-      };
-
+    function createLiElement(index, element) {
       return (
         <li
           key={index}
-          className="shrink-0 transition-all grid grid-cols-1 grid-rows-1 items-center justify-items-center group
-        scale-90 hover:scale-100 hover:cursor-pointer"
-          onMouseDown={(e) => clickingDownHandler(e)}
-          onMouseMove={(e) => clickingMoveHandler(e)}
-          onMouseUp={(e) => clickingUpHandler(e)}
-          onMouseLeave={(e) => clickingLeaveHandler(e)}
+          className="group/item grid w-[200px] shrink-0 snap-start grid-cols-1 grid-rows-1
+        items-center justify-items-center hover:cursor-pointer"
         >
-          {content}
-          <div className="group-hover:bg-[#0003] row-[1] col-[1] h-full w-full" />
+          <div className="z-0 col-[1] row-[1] scale-90 transition-all group-hover/item:scale-100 group-hover/item:brightness-75">
+            {element}
+          </div>
           <FontAwesomeIcon
             icon={faEye}
-            className="text-5xl group-hover:block row-[1] col-[1] hidden"
+            className="pointer-events-none z-[2] col-[1] row-[1] scale-0 text-5xl transition-all group-hover/item:block group-hover/item:scale-100"
           />
         </li>
       );
     }
-  }, [children, onClick]);
+  }, [children]);
 
-  //scroll functions
-  const [isDragging, setIsDragging] = useState(false);
-  const [prevXPos, setPrevXPos] = useState(0);
-  const hitEdgeStyle = "[&>*]:text-neutral-600";
-
-  const dragMouseUpHandler = () => {
-    setIsDragging(false);
-  };
-  const dragMouseMoveHandler = (e) => {
-    if (!isDragging) return;
-    holderRef.current.scrollLeft += prevXPos - e.clientX;
-    setPrevXPos(e.clientX);
-  };
-  const dragMouseDownHandler = (e) => {
-    setIsDragging(true);
-    setPrevXPos(e.clientX);
-  };
-
-  const scrollHandler = (e) => {
-    const scroll = e.target;
-    const scrollAmount = scroll.scrollLeft;
-
-    if (scrollAmount <= 0) {
-      controlsRef.current.left.classList.add(hitEdgeStyle);
-    } else {
-      controlsRef.current.left.classList.remove(hitEdgeStyle);
-    }
-    if (Math.ceil(scrollAmount) + scroll.clientWidth >= scroll.scrollWidth) {
-      controlsRef.current.right.classList.add(hitEdgeStyle);
-    } else {
-      controlsRef.current.right.classList.remove(hitEdgeStyle);
-    }
-  };
-
+  //add resize obcerver to scrollHolderRef
   useEffect(() => {
-    console.log(
-      "render",
-      holderRef.current.scrollWidth,
-      " > ",
-      holderRef.current.clientWidth
-    );
-    const resizeObserver = new ResizeObserver(() => {
-      if (holderRef.current.scrollWidth > holderRef.current.clientWidth) {
-        controlsRef.current.right.classList.remove("hidden");
-        controlsRef.current.left.classList.remove("hidden");
-        if (holderRef.current.scrollLeft <= 0)
-          controlsRef.current.left.classList.add(hitEdgeStyle);
+    //keeping a reference to the current scrollHolderRef
+    const currentScrollHolderRef = scrollHolderRef.current;
 
-        console.log(
-          "shown",
-          holderRef.current.scrollWidth,
-          " > ",
-          holderRef.current.clientWidth
-        );
+    const resizeObserver = new ResizeObserver(() => {
+      const scrollWidth = currentScrollHolderRef.clientWidth;
+      const contentWidth = currentScrollHolderRef.scrollWidth;
+
+      const styleGroup = "group/holder";
+
+      if (contentWidth > scrollWidth) {
+        holderRef.current.classList.add(styleGroup);
       } else {
-        controlsRef.current.right.classList.remove(hitEdgeStyle);
-        controlsRef.current.left.classList.remove(hitEdgeStyle);
-        controlsRef.current.left.classList.add("hidden");
-        controlsRef.current.right.classList.add("hidden");
-        console.log(
-          "hidden",
-          holderRef.current.scrollWidth,
-          " > ",
-          holderRef.current.clientWidth
-        );
+        holderRef.current.classList.remove(styleGroup);
       }
-      console.log("resize");
     });
-    resizeObserver.observe(holderRef.current);
+    resizeObserver.observe(currentScrollHolderRef);
     return () => {
-      resizeObserver.disconnect();
+      resizeObserver.unobserve(currentScrollHolderRef);
     };
-  }, [reRenderOnChange]);
-  function controlClickHandler(direction) {
-    const scrollAmount = 200;
-    if (direction === "left")
-      holderRef.current.scrollTo({
-        left:
-          Math.round(holderRef.current.scrollLeft / scrollAmount) *
-            scrollAmount -
-          scrollAmount,
-        behavior: "smooth",
-      });
-    else
-      holderRef.current.scrollTo({
-        left:
-          Math.round(holderRef.current.scrollLeft / scrollAmount) *
-            scrollAmount +
-          scrollAmount,
-        behavior: "smooth",
-      });
+  }, []);
+
+  function updateScrollControlsVisibility(scrollElement, controlsElementRef) {
+    //position is on the left of scroll and equals 0 in the start
+    const scrollPosition = scrollElement.scrollLeft;
+    const scrollWidth = scrollElement.clientWidth;
+    const contentWidth = scrollElement.scrollWidth;
+
+    const style = "group-hover/holder:translate-x-0";
+
+    //scroll is at the start
+    if (scrollPosition <= 0) {
+      controlsElementRef.left.classList.remove(style);
+    } else {
+      controlsElementRef.left.classList.add(style);
+    }
+    //scroll is at the end
+    if (Math.ceil(scrollPosition) + scrollWidth >= contentWidth) {
+      controlsElementRef.right.classList.remove(style);
+    } else {
+      controlsElementRef.right.classList.add(style);
+    }
   }
 
   return (
-    <div className="grid grid-cols-[auto_1fr_auto] grid-rows-1 bg-black">
-      <button
-        className="h-full bg-black p-1 hidden transition-all"
-        ref={(ref) => (controlsRef.current.left = ref)}
-        onClick={() => {
-          controlClickHandler("left");
-        }}
-      >
-        <FontAwesomeIcon icon={faCaretLeft} className={controlsClassName} />
-      </button>
-      <ul
-        className={`${className} scroll-m-0 no-scrollbar select-none`}
-        ref={holderRef}
-        onMouseDown={(e) => dragMouseDownHandler(e)}
-        onMouseMove={(e) => dragMouseMoveHandler(e)}
-        onMouseUp={(e) => dragMouseUpHandler(e)}
-        onMouseLeave={(e) => dragMouseUpHandler(e)}
-        onScroll={(e) => scrollHandler(e)}
-      >
-        {childrenRender}
-      </ul>
-      <button
-        className="h-full bg-black p-1 hidden transition-all"
-        ref={(ref) => (controlsRef.current.right = ref)}
-        onClick={() => {
-          controlClickHandler("right");
-        }}
-      >
-        <FontAwesomeIcon icon={faCaretRight} className={controlsClassName} />
-      </button>
+    <div
+      className="grid grid-cols-1 grid-rows-1 items-center overflow-hidden bg-black "
+      ref={holderRef}
+      onMouseEnter={() => {
+        if (
+          scrollHolderRef.current.scrollWidth >
+          scrollHolderRef.current.clientWidth
+        ) {
+          holderRef.current.classList.add("group/holder");
+        }
+        updateScrollControlsVisibility(
+          scrollHolderRef.current,
+          controlsRef.current,
+        );
+      }}
+      onMouseLeave={() => {
+        holderRef.current.classList.remove("group/holder");
+      }}
+    >
+      <Controls myRef={controlsRef} scrollHolder={scrollHolderRef}>
+        <ul
+          className="scroll-no-style z-[0] col-[1] row-[1] flex select-none snap-x snap-mandatory overflow-x-auto scroll-smooth"
+          ref={scrollHolderRef}
+          onScroll={(e) => {
+            updateScrollControlsVisibility(e.target, controlsRef.current);
+          }}
+        >
+          {childrenRender}
+        </ul>
+      </Controls>
     </div>
   );
 }
@@ -209,4 +133,39 @@ Default.propTypes = {
   className: PropTypes.string,
   controlsClassName: PropTypes.string,
   onClick: PropTypes.func,
+};
+
+function Controls({ children, scrollHolder, myRef }) {
+  const controlsStyle =
+    "text-5xl px-2 col-[1] row-[1] z-[1] self-center bg-[#00000050] h-full hover:bg-[#00000090] transition-all";
+
+  return (
+    <>
+      <button
+        ref={(ref) => (myRef.current.left = ref)}
+        className={`${controlsStyle} -translate-x-full place-self-start`}
+        onClick={() => {
+          scrollHolder.current.scrollBy(-1, 0);
+        }}
+      >
+        <FontAwesomeIcon icon={faCaretLeft} />
+      </button>
+      {children}
+      <button
+        ref={(ref) => (myRef.current.right = ref)}
+        className={`${controlsStyle} translate-x-full place-self-end group-hover/holder:translate-x-0`}
+        onClick={() => {
+          scrollHolder.current.scrollBy(1, 0);
+        }}
+      >
+        <FontAwesomeIcon icon={faCaretRight} />
+      </button>
+    </>
+  );
+}
+
+Controls.propTypes = {
+  children: PropTypes.element,
+  scrollHolder: PropTypes.object,
+  myRef: PropTypes.object,
 };
